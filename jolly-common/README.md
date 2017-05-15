0377f24e8c2b450c068456f1f83a5db7ed2d5e3a
berkeley <berkeley@jollycorp.com>
2017-05-15 16:13:29 +0800

    rename permission to umps

---
+++ jolly-common/README.md
@@ -0,0 +1,3 @@
+# insipid-common
+
+`insipid`项目SSM框架公共模块
---
+++ jolly-common/src/main/java/com/jolly/upms/common/base/BaseConstants.java
@@ -0,0 +1,10 @@
+package com.jolly.upms.common.base;
+
+/**
+ * 全局常量
+ * Created by berkeley on 2017/2/18.
+ */
+public class BaseConstants {
+
+
+}
